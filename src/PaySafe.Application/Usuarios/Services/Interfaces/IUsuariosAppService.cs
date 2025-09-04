2dc03612d295eed13c27c13b3af4573fbaa1cc3d
pereirajoyce <joyce_paiva32@hotmail.com>
2025-09-03 21:30:50 -0300

    usuarios crud completo

---
+++ src/PaySafe.Application/Usuarios/Services/Interfaces/IUsuariosAppService.cs
@@ -0,0 +1,13 @@
+﻿using PaySafe.Application.Usuarios.DataTransfer.Requests;
+using PaySafe.Application.Usuarios.DataTransfer.Responses;
+
+namespace PaySafe.Application.Usuarios.Services.Interfaces
+{
+    public interface IUsuariosAppService
+    {
+        Task<UsuarioResponse> InserirAsync(UsuarioInserirRequest request, CancellationToken cancellationToken);
+        Task<UsuarioResponse> EditarAsync(Guid guid, UsuarioEditarRequest request, CancellationToken cancellationToken);
+        Task<UsuarioResponse> RecuperarAsync(Guid guid, CancellationToken cancellationToken);
+        Task ExcluirAsync(Guid guid, CancellationToken cancellationToken);
+    }
+}
--- src/PaySafe.Domain/Usuarios/Services/Interfaces/IUsuariosService.cs
+++ src/PaySafe.Domain/Usuarios/Services/Interfaces/IUsuariosService.cs
@@ -1,8 +1,14 @@
-﻿using PaySafe.Domain.Usuarios.Repositories;
+﻿using PaySafe.Domain.Usuarios.Commands;
+using PaySafe.Domain.Usuarios.Entities;
 
 namespace PaySafe.Domain.Usuarios.Services.Interfaces
 {
     public interface IUsuariosService
     {
+        Task<Usuario> ValidarAsync(Guid guid, CancellationToken cancellationToken);
+        Task<Usuario> RecuperarAsync(Guid guid, CancellationToken cancellationToken);
+        Task<Usuario> InserirAsync(UsuarioCommand command, CancellationToken cancellationToken);
+        Task<Usuario> EditarAsync(Guid guid, UsuarioEditarCommand command, CancellationToken cancellationToken);
+        Task ExcluirAsync(Guid guid, CancellationToken cancellationToken);
     }
 }
