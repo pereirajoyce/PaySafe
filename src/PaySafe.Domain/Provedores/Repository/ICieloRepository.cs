b5e2e896e6da4f457ce92a71fe1396b94bc53680
Joy <joyce_paiva32@hotmail.com>
2025-09-23 22:35:24 -0300

    Integracao inicial provedor Cielo

---
+++ src/PaySafe.Domain/Provedores/Repository/ICieloRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace PaySafe.Domain.Provedores.Repository
+{
+    public interface ICieloRepository
+    {
+        Task<HttpResponseMessage> ConsultarBinDoCartaoAsync(string bin, CancellationToken cancellationToken);
+    }
+}
---
+++ src/PaySafe.Infrastructure/Provedores/Cielo/CieloRepository.cs
@@ -0,0 +1,25 @@
+﻿using PaySafe.Application.Provedores.DataTransfer;
+using PaySafe.Domain.Provedores.Repository;
+using System.Net.Http.Json;
+
+namespace PaySafe.Infrastructure.Provedores.Cielo
+{
+    public class CieloRepository(IHttpClientFactory httpClientFactory) : ICieloRepository
+    {
+        private readonly HttpClient _httpClient = httpClientFactory.CreateClient("Cielo.Api");
+
+        public async Task<HttpResponseMessage> ConsultarBinDoCartaoAsync(string bin, CancellationToken cancellationToken)
+        {
+            var response = await _httpClient.GetAsync($"/1/cardBin/{bin}", cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Erro na consulta Cielo: {response.StatusCode}, {content}");
+            }
+
+            return response;
+
+        }
+    }
+}
